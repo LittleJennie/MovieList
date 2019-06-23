757b91feb65b5c264ffdc19229c43c2282c5e1f9
Jennie Zeng <jenniezeng1@gmail.com>
2019-06-23 15:20:44 -0700

    server side structure set up

---
+++ server/controller/index.js
@@ -0,0 +1,38 @@
+const models = require('../models/index.js');
+
+module.exports = {
+
+    all: {
+        get: (req, res) => {
+
+        }, 
+
+        post: (req, res) => {
+            
+        }
+    }, 
+
+    towatch: {
+        get: (req, res) => {
+
+        }, 
+
+        post: (req, res) => {
+
+        }, 
+
+        delete: (req, res) => {
+
+        }
+    }, 
+
+    watched: {
+        get: (req, res) => {
+
+        }, 
+
+        post: (req, res) => {
+
+        }
+    }
+}
--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,18 @@
 const routes = require('express').Router();
 const controller = require('./controller/index.js');
 
-routes.get('/', controller);
+routes.get('/all', controller.all.get);
+routes.post('/all', controller.all.post);
+
+routes.get('/towatch', controller.towatch.get);
+routes.post('/towatch', controller.towatch.post);
+routes.delete('/towatch', controller.towatch.delete);
+
+routes.get('/watched', controller.watched.get);
+routes.post('/watched', controller.watched.post);
+
+// add route to individual id as well
+
+
 
 module.exports = routes;
