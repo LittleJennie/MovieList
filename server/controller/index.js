ac462a21b64024471fd663e97df7b470505eaa74
Jennie Zeng <jenniezeng1@gmail.com>
2019-06-23 17:38:35 -0700

    controller index.js get post put methods implemented

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,38 +1,91 @@
-const db = require('../db/');
+const db = require('../db/index.js');
 
 module.exports = {
-
+    //localhost:3000/api/movies/all
     all: {
         get: (req, res) => {
-
+            db.Movies.findAll()
+                .then((movies) => {
+                    res.status(200).json(movies);
+                })
+                .catch((err) => {
+                    res.status(400).send('something went wront');
+                });
         }, 
 
         post: (req, res) => {
+            // assuming req.body is formated well
+            db.Movies.findOrCreate({where: {imdbId: req.body.imdbId}, default: {
+                title: req.body.title,
+                releaseDate: req.body.releaseDate,
+                vote: req.body.vote,
+                overviews: req.body.overviews,
+                img: req.body.img,
+                towatch: req.body.towatch
+            }})
+                .spread((movie, created) => {
+                    res.sendStatus(created ? 201 : 200);
+                });
+        }, 
 
+        put: (req, res) => {
+
+            // find the movie first, when sucess, update the record
+            db.Movies.findAll({where: {imdbId: req.body.imdbId}})
+                .then((movie) => {
+                    if (movie.towatch) {
+                        movie.update(
+                            {towatch: false}, 
+                            {returning: true}
+                        )
+                        .spread((affectedRow, updatedMovie) => {
+                            // The promise returns an array with one or two elements. 
+                            // The first element is always the number of affected rows, 
+                            // while the second element is the actual affected rows 
+                            // (only supported in postgres with options.returning true.)
+
+                            res.status(201).json(updatedMovie);
+
+                        })
+                    } else {
+                        movie.update(
+                            {towatch: true},
+                            {returning: true}
+                        )
+                        .spread((affectedRow, updatedMovie) => {
+                            res.status(201).json(updatedMovie);
+                        })
+                    }
+                })
+                .catch((err) => {
+                    res.status(400).send('movie not found');
+                });
         }
     }, 
 
+    //localhost:3000/api/movies/towatch
     towatch: {
         get: (req, res) => {
-
-        }, 
-
-        post: (req, res) => {
-
-        }, 
-
-        delete: (req, res) => {
-
+            db.Movies.findAll({where: {towatch: true}})
+            .then((movies) => {
+                res.status(200).json(movies);
+            })
+            .catch((err) => {
+                res.status(400).send('something went wront');
+            }); 
         }
     }, 
 
+    //localhost:3000/api/movies/watched
     watched: {
         get: (req, res) => {
-
-        }, 
-
-        post: (req, res) => {
-
+            db.Movies.findAll({where: {towatch: false}})
+            .then((movies) => {
+                res.status(200).json(movies);
+            })
+            .catch((err) => {
+                res.status(400).send('something went wront');
+            }); 
         }
     }
 }
--- server/routes.js
+++ server/routes.js
@@ -3,13 +3,12 @@ const controller = require('./controller/index.js');
 
 routes.get('/all', controller.all.get);
 routes.post('/all', controller.all.post);
+routes.put('/all', controller.all.put);
+// add route to delete a movie as well --> need to implement front end feature as well 
 
 routes.get('/towatch', controller.towatch.get);
-routes.post('/towatch', controller.towatch.post);
-routes.delete('/towatch', controller.towatch.delete);
 
 routes.get('/watched', controller.watched.get);
-routes.post('/watched', controller.watched.post);
 
 // add route to individual id as well
 
